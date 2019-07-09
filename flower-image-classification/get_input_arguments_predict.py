673b66dfaeead7292436818bf0d0575bf5991c6e
Mert Ozturk <mert1ozturk@gmail.com>
2019-07-09 17:35:27 +0300

    minor styling update in the doc

--- flower-image-classification/get_input_arguments_predict.py
+++ flower-image-classification/get_input_arguments_predict.py
@@ -1,5 +1,7 @@
+# ------------------------------
 # AUTHOR: MERT ÖZTÜRK
 # Contact: mert1ozturk@gmail.com
+# ------------------------------
 
 import argparse
 
@@ -9,4 +11,4 @@ def get_input_arguments_predict():
     parser.add_argument("--categories", type=str, default="cat_to_name.json", help="The file that contains name of the categories.")
     parser.add_argument("--gpu", type=bool, default=True, help="Choose between GPU and CPU. Note: GPU is highly advised.")
     parser.add_argument('--image', type=str, default='test_image.jpg', help="Type in the folder that contains the images that you would like to make predictions on.")
-    parser.add_argument('--topk', type=int, default=5, help="Type in the number of top K classes to be returned.") 
+    parser.add_argument('--topk', type=int, default=5, help="Type in the number of top K classes to be returned.")
