526fa8069c4043b3cac8fc34940a40907c1dd844
Mert Ozturk <mert1ozturk@gmail.com>
2019-07-09 21:42:56 +0300

    add predict method in utilities

--- flower-image-classification/train.py
+++ flower-image-classification/train.py
@@ -1,21 +1,28 @@
-def classifier_create(model, n_input_units, n_hidden_units1, n_hidden_units2, dropout_rate):
+def predict(image, model, topk, gpu):
 
-    # No need
-    for p in model.parameters():
-        p.requires_grad = False
+    model.eval()
 
-    from collections import OrderedDict
-    classifier = nn.Sequential(OrderedDict([
-                              ('fc1', nn.Linear(n_input_units, n_hidden_units1)),
-                              ('relu', nn.ReLU()),
-                              ('dropout1', nn.Dropout(dropout_rate)),
-                              ('fc2', nn.Linear(n_hidden_units1, n_hidden_units2)),
-                              ('relu', nn.ReLU()),
-                              ('dropout2', nn.Dropout(dropout_rate)),
-                              ('fc3', nn.Linear(n_hidden_units2, 102)),
-                              ('output', nn.LogSoftmax(dim=1))
-                              ]))
+    if gpu==True:
+        model.to("cuda")
+    else:
+        model.cpu()
 
-    model.classifier = classifier
+    with torch.no_grad():
+        output = model.forward(image)
 
-    return model
+    probabilities = torch.exp(output)
+    probabilities_top = probabilities.topk(topk)[0]
+    indexes_top = probabilities.topk(topk)[1]
+
+    probabilities_top_list = np.array(probabilities_top)[0]
+    indexes_top_list = np.array(indexes_top[0])
+
+    class_to_idx = model.class_to_idx
+
+    indexes_to_classes = {y: x for x, y in class_to_idx.items()}
+
+    classes_top_list = []
+    for item in indexes_top_list:
+        classes_top_list += [indexes_to_classes[item]]
+
+    return probabilities_top_list, classes_top_list
--- flower-image-classification/utilities.py
+++ flower-image-classification/utilities.py
@@ -225,3 +225,33 @@ def classifier_create(model, n_input_units, n_hidden_units1, n_hidden_units2, dr
     model.classifier = classifier
 
     return model
+
+
+def predict(image, model, topk, gpu):
+
+    model.eval()
+
+    if gpu==True:
+        model.to("cuda")
+    else:
+        model.cpu()
+
+    with torch.no_grad():
+        output = model.forward(image)
+
+    probabilities = torch.exp(output)
+    probabilities_top = probabilities.topk(topk)[0]
+    indexes_top = probabilities.topk(topk)[1]
+
+    probabilities_top_list = np.array(probabilities_top)[0]
+    indexes_top_list = np.array(indexes_top[0])
+
+    class_to_idx = model.class_to_idx
+
+    indexes_to_classes = {y: x for x, y in class_to_idx.items()}
+
+    classes_top_list = []
+    for item in indexes_top_list:
+        classes_top_list += [indexes_to_classes[item]]
+
+    return probabilities_top_list, classes_top_list
