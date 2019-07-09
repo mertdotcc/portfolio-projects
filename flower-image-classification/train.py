8336409bddee43806204cff9bade546c8d6ce535
Mert Ozturk <mert1ozturk@gmail.com>
2019-07-09 22:41:47 +0300

    bug free (so far)

--- flower-image-classification/train.py
+++ flower-image-classification/train.py
@@ -31,3 +31,46 @@ parser.add_argument("--dropout", type=float, default=0.5, help="Type in the valu
 parser.add_argument("--gpu", type=bool, default=True, help="Choose between GPU and CPU. Note: GPU is highly advised.")
 parser.add_argument("--categories", type=str, default="cat_to_name.json", help="The file that contains name of the categories.")
 parser.add_argument("--savedir", type=str, default="checkpoints", help="Type in the location you want the checkpoints to be saved at.")
+
+user_input = parser.parse_args()
+
+dir = user_input.dir
+model_architecture = user_input.model
+checkpoint = user_input.checkpoint
+learning_rate = user_input.lr
+n_hidden_units = user_input.hidden
+epoch = user_input.epoch
+batch_size = user_input.batch
+n_steps = user_input.step
+dropout_rate = user_input.dropout
+gpu = user_input.gpu
+categories = user_input.categories
+save_dir = user_input.savedir
+
+# We are handling the data loading and preprocessing part
+training_loader, validation_loader, test_loader, training_data, validation_data, test_data = data_load(dir)
+
+if model_architecture=="vgg19":
+    model = models.vgg19(pretrained=True)
+elif model_architecture=="alexnet":
+    model = models.alexnet(pretrained=True)
+elif model_architecture=="densenet121":
+    model = models.densenet121(pretrained=True)
+else:
+    print("Oops. This program doesn't support {}. Please choose between vgg19, alexnet, densenet121".format(model_architecture))
+
+n_input_units = model.classifier[0].in_features
+classifier_create(model, n_input_units, n_hidden_units, dropout_rate)
+
+# Since we are using Softmax
+criterion = nn.NLLLoss()
+optimizer = optim.Adam(model.classifier.parameters(), learning_rate)
+
+# Train the model
+model, optimizer = model_train(model, epoch, optimizer, criterion, gpu, training_loader, validation_loader)
+
+# Test the model
+model_test(model, test_loader, gpu)
+
+# Save the model for later use
+model_save(model, epoch, optimizer, training_data, save_dir)
--- flower-image-classification/utilities.py
+++ flower-image-classification/utilities.py
@@ -204,7 +204,7 @@ def get_validation(model, validation_loader, criterion, gpu):
     return valvalidation_lossid_loss, accuracy
 
 
-def classifier_create(model, n_input_units, n_hidden_units1, n_hidden_units2, dropout_rate):
+def classifier_create(model, n_input_units, n_hidden_units, dropout_rate):
 
     # No need
     for p in model.parameters():
@@ -212,13 +212,10 @@ def classifier_create(model, n_input_units, n_hidden_units1, n_hidden_units2, dr
 
     from collections import OrderedDict
     classifier = nn.Sequential(OrderedDict([
-                              ('fc1', nn.Linear(n_input_units, n_hidden_units1)),
+                              ('fc1', nn.Linear(n_input_units, n_hidden_units)),
                               ('relu', nn.ReLU()),
-                              ('dropout1', nn.Dropout(dropout_rate)),
-                              ('fc2', nn.Linear(n_hidden_units1, n_hidden_units2)),
-                              ('relu', nn.ReLU()),
-                              ('dropout2', nn.Dropout(dropout_rate)),
-                              ('fc3', nn.Linear(n_hidden_units2, 102)),
+                              ('dropout', nn.Dropout(dropout_rate)),
+                              ('fc2', nn.Linear(n_hidden_units, 102)),
                               ('output', nn.LogSoftmax(dim=1))
                               ]))
 
