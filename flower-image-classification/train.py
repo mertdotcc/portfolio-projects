eb650208e58dca05f2d6154698bcb57e10381805
Mert Ozturk <mert1ozturk@gmail.com>
2019-07-10 16:03:40 +0300

    LOW test accuracy!

--- flower-image-classification/train.py
+++ flower-image-classification/train.py
@@ -24,13 +24,13 @@ parser.add_argument("--model", type=str, default="vgg19", help="Type in the NN m
 parser.add_argument("--checkpoint", type=str, default="checkpoint.pth", help="Type in the directory of your pretrained model if you have any.")
 parser.add_argument("--lr", type=float, default=0.001, help="Type in the learning rate you want.")
 parser.add_argument("--hidden", type=int, default=500, help="Type in the number of hidden units you want.")
-parser.add_argument("--epoch", type=int, default=5, help="Type in the number of epoch you want.")
+parser.add_argument("--epoch", type=int, default=1, help="Type in the number of epoch you want.")
 parser.add_argument("--batch", type=int, default=64, help="Type in the value of the batch size you want.")
 parser.add_argument("--step", type=int, default=3, help="Type in the number of train steps you want.")
 parser.add_argument("--dropout", type=float, default=0.5, help="Type in the value of dropouts that will be used in the training set.")
 parser.add_argument("--gpu", type=bool, default=True, help="Choose between GPU and CPU. Note: GPU is highly advised.")
 parser.add_argument("--categories", type=str, default="cat_to_name.json", help="The file that contains name of the categories.")
-parser.add_argument("--savedir", type=str, default="checkpoints", help="Type in the location you want the checkpoints to be saved at.")
+parser.add_argument("--savedir", type=str, default="checkpoint.pth", help="Type in the location you want the checkpoints to be saved at.")
 
 user_input = parser.parse_args()
 
@@ -73,4 +73,4 @@ model, optimizer = model_train(model, epoch, optimizer, criterion, gpu, training
 model_test(model, test_loader, gpu)
 
 # Save the model for later use
-model_save(model, epoch, optimizer, training_data, save_dir)
+trained_model = model_save(model, epoch, optimizer, training_data, save_dir)
